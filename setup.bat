d6c44d87270e7bbdb17a8351c08201614af04449
ChheanSilapin <silapinchhean@gmail.com>
2025-12-20 11:04:23 +0700

    Update GitHub URL

--- setup.bat
+++ setup.bat
@@ -35,8 +35,8 @@ if errorlevel 1 (
 :: Clone if not already in project folder
 if not exist "main.py" (
     echo [INFO] Cloning project from GitHub...
-    git clone https://github.com/MyWork22-creator/VideoGenerate_Tools.git
-    cd VideoGenerate_Tools
+    git clone https://github.com/ChheanSilapin/getContentGenerateVideo.git
+    cd getContentGenerateVideo
 )
 
 :: Check if uv is installed
